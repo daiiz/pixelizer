35fa79d3f6487a329fe56037b82f4bec858f7bbd
daiiz <da.iz.mg@gmail.com>
2019-06-23 13:53:10 +0900

    できてきた

--- encode.py
+++ encode.py
@@ -2,13 +2,17 @@
 import sys, os
 import numpy as np
 from svg_writer import svg_a, svg_b
+from html_writer import html_a
 from PIL import Image
 
-def to_matrix(raw_image_path):
+def to_matrix(raw_image_path, dpr=1):
   im = Image.open(raw_image_path)
-  arr = np.array(im)
-  print('shape of raw image:', np.shape(arr))
-  return arr
+  (height, width, depth) = np.shape(np.array(im))
+  # im = im.resize((100,100), Image.LANCZOS)
+  if width >= 500 or height >= 500:
+    im = im.resize((int(width / 5), int(height / 5)), Image.LANCZOS)
+  im.save('./tmp/a.jpg')
+  return np.array(im)
 
 if __name__ == '__main__':
   print(sys.version_info)
@@ -20,10 +24,14 @@ if __name__ == '__main__':
   if not os.path.exists(raw_image_path):
     print('Input image is not found.')
     exit()
-  mat = to_matrix(raw_image_path)
+  mat = to_matrix(raw_image_path, dpr=10)
   
-  svg_text = svg_b(mat, dpr=1)
+  # text = svg_b(mat)
+  text = html_a(mat)
+  file_name = raw_image_path.split('/').pop().split('.')[0]
 
-  svg_file_name = raw_image_path.split('/').pop().split('.')[0]
-  with open('./svg/' + svg_file_name + '.b.svg', mode='w') as f:
-    f.write(svg_text)
+  # with open('./svg/' + file_name + '.b.svg', mode='w') as f:
+    # f.write(text)
+
+  with open('./html/' + file_name + '.a.svg', mode='w') as f:
+    f.write(text)
--- svg_writer.py
+++ svg_writer.py
@@ -2,54 +2,56 @@ import numpy as np
 
 svg_tail = '</svg>'
 
-def svg_head(mat, dpr=1):
+def svg_head(mat, pixel_width):
   (height, width, depth) = np.shape(mat)
   return ''.join([
     '<svg xmlns="http://www.w3.org/2000/svg" '
-    'width="' + str(width / dpr) + '" ',
-    'height="' + str(height / dpr) + '" ',
-    'viewBox="0 0 ' + str(width / dpr) + ' ' + str(height / dpr) + '">'
+    'width="' + str(width * pixel_width) + '" ',
+    'height="' + str(height * pixel_width) + '" ',
+    'viewBox="0 0 ' + str(width * pixel_width) + ' ' + str(height * pixel_width) + '">'
   ])
 
-def rect_1x1px(x, y, r, g, b, dpr=1):
-  p = int(1 / dpr)
+def rect_1x1px(x, y, r, g, b):
   return ''.join([
-    '<rect width="'+ str(p) +'" height="'+ str(p) +'" ',
+    '<rect width="'+ str(1) +'" height="'+ str(1) +'" ',
     'x="'+ str(x) +'" ',
     'y="'+ str(y) +'" ',
     'fill="rgb('+ str(r) +', '+ str(g) +', '+ str(b) +')">'
     '</rect>'
   ])
 
-def rect_wx1px(x, y, w, r, g, b, dpr=1):
+def rect_wx1px(x, y, w, r, g, b):
+  # if r >= 254 and g >= 254 and b >= 254:
+  #   return ''
+  # color = '#' + hex(r)[2:4] + hex(g)[2:4] + hex(b)[2:4]
   return ''.join([
-    '<rect width="'+ str(int(w / dpr)) +'" height="'+ str(int(1 / dpr)) +'" ',
+    '<rect width="'+ str(w) +'" height="'+ str(1) +'" ',
     'x="'+ str(x) +'" ',
     'y="'+ str(y) +'" ',
     'fill="rgb('+ str(r) +', '+ str(g) +', '+ str(b) +')">'
     '</rect>'
   ])
 
-def svg_a(matrix, dpr=1):
+def svg_a(matrix):
   svg_body = []
   idx_row = 0
   for row in matrix:
     idx_col = 0
     for col in row:
-      (r, g, b, a) = col
-      svg_body.append(rect_1x1px(idx_col, idx_row, r, g, b, 1))
+      (r, g, b) = col
+      svg_body.append(rect_1x1px(idx_col, idx_row, r, g, b))
       idx_col += 1
     idx_row += 1
   body = '\n'.join(svg_body)
-  return '\n'.join([svg_head(matrix, dpr), body, svg_tail])
+  return '\n'.join([svg_head(matrix), body, svg_tail])
 
-def svg_b(matrix, dpr=1):
+def svg_b(matrix):
   svg_body = []
   for idx_row, row in enumerate(matrix):
     rgb = [None, None, None]
     w_px = 0
     for idx_col, col in enumerate(row):
-      (r, g, b, a) = col
+      (r, g, b) = col
       if idx_col == 0:
         rgb[0] = r
         rgb[1] = g
@@ -58,14 +60,18 @@ def svg_b(matrix, dpr=1):
         w_px += 1
       else:
         if rgb[0] is not None:
-          svg_body.append(rect_wx1px(idx_col - w_px, idx_row, w_px + 1, rgb[0], rgb[1], rgb[2], 1))
+          rect = rect_wx1px(idx_col - w_px, idx_row, w_px + 1, rgb[0], rgb[1], rgb[2])
+          if len(rect) > 0:
+            svg_body.append(rect)
         rgb[0] = r
         rgb[1] = g
         rgb[2] = b
         w_px = 0
       if idx_col == len(row) - 1:
         if rgb[0] is not None:
-          svg_body.append(rect_wx1px(idx_col - w_px, idx_row, w_px + 1, rgb[0], rgb[1], rgb[2], 1))
+          rect = rect_wx1px(idx_col - w_px, idx_row, w_px + 1, rgb[0], rgb[1], rgb[2])
+          if len(rect) > 0:
+            svg_body.append(rect)
 
   body = '\n'.join(svg_body)
-  return '\n'.join([svg_head(matrix, dpr), body, svg_tail])
+  return '\n'.join([svg_head(matrix), body, svg_tail])
