d3dfa0c02c1e4dc93e326702a6a73858962711fa
daiiz <da.iz.mg@gmail.com>
2019-06-23 18:38:27 +0900

    impl html_a

--- encode.py
+++ encode.py
@@ -5,33 +5,34 @@ from svg_writer import svg_a, svg_b
 from html_writer import html_a
 from PIL import Image
 
-def to_matrix(raw_image_path, dpr=1):
+def to_matrix(raw_image_path, file_name):
   im = Image.open(raw_image_path)
   (height, width, depth) = np.shape(np.array(im))
-  # im = im.resize((100,100), Image.LANCZOS)
-  if width >= 500 or height >= 500:
+  if (width >= 500 or height >= 500) and (not raw_image_path.endswith('.png')):
     im = im.resize((int(width / 5), int(height / 5)), Image.LANCZOS)
-  im.save('./tmp/a.jpg')
+  im.save('./tmp/'+ file_name +'.bmp')
+  im = Image.open('./tmp/'+ file_name +'.bmp')
   return np.array(im)
 
 if __name__ == '__main__':
-  print(sys.version_info)
   args = sys.argv
   if len(args) <= 1 or len(args[1]) == 0:
     print('Input image is required.')
     exit()
   raw_image_path = args[1]
+  
+  file_name = raw_image_path.split('/').pop().split('.')[0]
   if not os.path.exists(raw_image_path):
     print('Input image is not found.')
     exit()
-  mat = to_matrix(raw_image_path, dpr=10)
+  mat = to_matrix(raw_image_path, file_name)
   
-  # text = svg_b(mat)
-  text = html_a(mat)
-  file_name = raw_image_path.split('/').pop().split('.')[0]
+  px_str = '@'
+  if (len(args) > 2):
+    px_str = args[2]
 
-  # with open('./svg/' + file_name + '.b.svg', mode='w') as f:
-    # f.write(text)
+  print(file_name, px_str)
+  text = html_a(mat, px_str)
 
   with open('./html/' + file_name + '.a.svg', mode='w') as f:
     f.write(text)
--- html_writer.py
+++ html_writer.py
@@ -3,44 +3,31 @@ from svg_writer import svg_head
 
 def html_head(mat):
   return ''.join([
-    # '<!doctype html>',
-    # '<html>',
-    # '<head>',
-    # '<meta charset="utf-8" />',
-    # '<link rel="stylesheet" href="./main.css" />',
-    # '</head>',
-    # '<body>',
     svg_head(mat, 6),
     '<foreignObject xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" x="0" y="0">'
     '<html xmlns="http://www.w3.org/1999/xhtml">',
-    # '<link rel="stylesheet" href="./main.css" />',
     '<style type="text/css">',
     '.dot {position: relative;font-size: 10px;width: 6px;height: 6px;margin: 0;padding: 0;display: inline-block;vertical-align: top; -webkit-font-smoothing: antialiased; font-weight:bold;} ',
-    # '.dot-bg {width:5px; height:5px; top:0; left:0; display:block;position:absolute;}',
     '.row {line-height: 6px; padding: 0; display: flex; width:100%;} ',
     '.main{position: relative;} ',
     '</style>',
     '<div class="main">'
   ])
 
-html_tail = '</div></html></foreignObject></svg>' #'</body></html>'
+html_tail = '</div></html></foreignObject></svg>'
 
-def rect_1x1px(r, g, b):
+def rect_1x1px(r, g, b, px_str='@'):
   color = 'rgb('+ str(r) +', '+ str(g) +', '+ str(b) +')'
   colorT = 'rgb('+ str(r) +', '+ str(g) +', '+ str(b) +', .8)' # .5
-  # ■, ●, 飯, @, 新, 針
-  # '<span class="dot" style="color: '+ color +'; text-shadow: -1px 0px 4px '+ colorT +';">車</span>'
-  # return '<rect width="7" height="7" vector-effect="non-scaling-stroke" fill="'+ color +'"></rect>'
-  return '<span class="dot" style="color: '+ color +'; text-shadow: 0px 0px 2px '+ colorT +';">0@</span>' # 針, text-shadow: -1px 0px 2px '+ colorT +';
-  # <span class="dot-bg" style="background-color: '+ colorT +'"></span>
+  return '<span class="dot" style="color: '+ color +'; text-shadow: 0px 0px 2px '+ colorT +';">'+ px_str +'</span>'
 
-def html_a(matrix):
+def html_a(matrix, px_str):
   body = []
   for idx_row, row in enumerate(matrix):
     body.append('<div class="row">')
     for idx_col, col in enumerate(row):
       (r, g, b) = col
-      body.append(rect_1x1px(r, g, b))
+      body.append(rect_1x1px(r, g, b, px_str))
     body.append('</div>')
   body = '\n'.join(body)
   return '\n'.join([html_head(matrix), body, html_tail])
